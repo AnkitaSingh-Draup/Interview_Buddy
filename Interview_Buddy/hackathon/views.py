469bc5dded03551a7d6f900e7d29c33c78889cc9
ankita <ankita.singh@draup.com>
2022-09-15 12:59:00 +0530

    Table creation

--- Interview_Buddy/hackathon/views.py
+++ Interview_Buddy/hackathon/views.py
@@ -5,12 +5,12 @@ from .models import Job
 # Create your views here.
 
 
-@api_view(['POST'])
+@api_view(['GET'])
 @permission_classes((AllowAny,))
 def get_job_skill_role(request):
     response = {'status': 'success', 'result': {}, 'errors': []}
     try:
-        if request.method == 'POST':
+        if request.method == 'GET':
             data = Job.objects.filter().values()
 
             response['result'] = data
